840496a1536b5d06a61dff01986ef762bd320bf6
Nulo <git@nulo.in>
2023-12-09 13:45:11 -0300

    borrar link duplicado a github

--- frontend/src/lib/routes/Home.svelte
+++ frontend/src/lib/routes/Home.svelte
@@ -4,17 +4,13 @@
   import Container from "../components/Container.svelte";
 </script>
 
-<main class="mx-auto prose dark:prose-invert">
+<main class="prose mx-auto dark:prose-invert">
   <Container>
-    <div class="py-5 px-6">
+    <div class="px-6 py-5">
       <h1>Archivo de portales de datos abiertos</h1>
       <p>
         Esta herramienta permite ver datos en archivos de portales de datos
-        abiertos de <a
-          href="https://github.com/catdevnull/transicion-desordenada-diablo/"
-          rel="noopener">transicion-desordenada-diablo</a
-        >
-        (un mejor nombre sería genial), creada en el marco de
+        abiertos, creada en el marco de
         <a href="https://bit.ly/CartaDatosAbiertos">un pedido hecho</a> al gobierno
         entrante el 10 de diciembre de 2023 por garantizar el mantenimiento de las
         políticas de datos públicos en Argentina.
@@ -27,7 +23,7 @@
               "https://archivos.nulo.ar/dump-2023-12-08/",
             ),
           })}
-          class="flex items-center justify-center px-4 py-2 text-xl font-medium text-white transition-colors duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:shadow-outline focus:outline-none text-center"
+          class="focus:shadow-outline flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 text-center text-xl font-medium text-white transition-colors duration-200 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2"
         >
           Acceder al archivo creado el 8 de diciembre de 2023
         </a>

5a62f78753382536f10b63794ad9799306064774
Nulo <git@nulo.in>
2023-12-10 01:46:40 -0300

    home: cantidad de portales y datasets en dump

--- frontend/src/lib/routes/Home/Portal.svelte
+++ frontend/src/lib/routes/Home/Portal.svelte
@@ -6,23 +6,25 @@
 
 <li>
   <a
-    class="shadow-glow flex justify-between px-6 py-5 shadow-blue-300 hover:bg-gray-50 dark:hover:bg-gray-700"
+    class="flex justify-between px-6 py-5 shadow-glow shadow-blue-300 hover:bg-gray-50 dark:hover:bg-gray-700"
     href={inject(routes.Dump, {
       dumpUrl: encodeURIComponent("https://archivos.nulo.ar/dump-2023-12-08/"),
     })}
   >
     <div>
       <h3 class="text-lg">8 de diciembre de 2023</h3>
-      <!-- <span
+      <span
         class="relative ml-1 inline-flex items-center rounded-full border border-current px-2 py-1 text-xs font-semibold text-blue-800 dark:text-blue-400"
       >
-        <span>portales</span>
+        <!-- find . -maxdepth 1 -mindepth 1 -type d | wc -l -->
+        <span>43 portales</span>
       </span>
       <span
         class="relative ml-1 inline-flex items-center rounded-full border border-current px-2 py-1 text-xs font-semibold text-blue-800 dark:text-blue-400"
       >
-        <span>datasets</span>
-      </span> -->
+        <!-- jq '.dataset | length' */data.json | awk '{s+=$1} END {print s}' -->
+        <span>3277 datasets</span>
+      </span>
     </div>
     <ArrowForward
       fill="currentColor"
